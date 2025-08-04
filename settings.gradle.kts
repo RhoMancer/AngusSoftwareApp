9f2b6d123a0511f3608217ba706f2534547c6ca5
RhoMan¢er <rhomancer-github@mail.angussoftware.com>
2025-08-04 14:53:48 -0500

    feat: update dependencies and refine App layout
    • bump AngusTheme to version 0.0.2.13 for latest theming updates
    • disable `mavenLocal()` in `settings.gradle.kts`
    • clean up redundant parameters in scaffold configurations

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,7 +30,7 @@ dependencyResolutionManagement {
                 includeGroupAndSubgroups("com.google")
             }
         }
-        mavenLocal()
+//        mavenLocal()
         mavenCentral()
         maven {
             url = uri("https://maven.pkg.github.com/RhoMancer/Angus-Software-Theming")
