13e6a5b7641c99131266481d8e5a00966de64a4d
RhoMan¢er <rhomancer-github@mail.angussoftware.com>
2025-08-18 18:53:30 -0500

    feat: refine adaptive layout with density-based widthDp
    • use LocalDensity to convert container width from px to dp
    • improve adaptive size class determination with accurate dp values

--- composeApp/src/androidMain/kotlin/dev/angussoftware/app/WindowAdaptiveInfo.android.kt
+++ composeApp/src/androidMain/kotlin/dev/angussoftware/app/WindowAdaptiveInfo.android.kt
@@ -2,18 +2,23 @@ package dev.angussoftware.app
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 /**
  * Android implementation of currentWindowAdaptiveInfo using LocalConfiguration
  */
 @Composable
 actual fun currentWindowAdaptiveInfo(): WindowAdaptiveInfo {
-    val widthDp = LocalWindowInfo.current.containerSize
-    
-    val widthSizeClass = WindowWidthSizeClass.fromWidth(widthDp.width)
-    
+    val widthPx = LocalWindowInfo.current.containerSize.width
+    val density = LocalDensity.current.density
+
+    val widthDp = widthPx / density
+
+    val widthSizeClass = WindowWidthSizeClass.fromWidth(widthDp.toInt())
+
     return WindowAdaptiveInfo(widthSizeClass)
 }
 
